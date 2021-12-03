120bad3e0656188556b6a3aea4ff3756a10742e4
Dhruv <dhruv.bhatia246@gmail.com>
2021-12-03 18:12:16 +0530

    baseURL

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,9 +1,7 @@
-const baseURL = 
-  location.hostname === "localhost" 
-    ? "http://localhost:2000" 
-    : "https://e-commerce-rest-backend.herokuapp.com";
-
+const baseURL = "https://e-commerce-rest-backend.herokuapp.com";
+  
 export const api = `${baseURL}/api`;
+
 export const generatePublicUrl = (fileName) => {
   return `${baseURL}/public/${fileName}`
 }
